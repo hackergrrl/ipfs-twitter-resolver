2c224a8d392ab56ba93f92e23d87124aa90166f4
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-05-02 14:48:05 +0200

    works!

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var params = qs.parse(location.search)
 
 if (!params.key) {
   document.write('need to specify "key" as a url param')
+  document.write('<br><br>')
+  document.write('e.g. ?key=/twitter/noffle/blog')
   return
 }
 
@@ -13,9 +15,16 @@ if (addr.length !== 4) return document.write('malform addr')
 if (addr[0] !== '') return document.write('malformed addr')
 if (addr[1] !== 'twitter') return document.write('malformed addr')
 
+document.write('Loading...<br><br>')
+
 kv(addr[2], addr[3], function (err, value) {
-  if (err) return console.error('err', err)
-  document.write('value', value)
+  if (err) return document.write('err', err)
+  document.write('<a href=https://ipfs.io' + value + '>' + value + '</a>')
+  document.write('<br><br>redirecting..')
+
   // TODO: validate that value is an /ipfs or /ipns multiaddr
-  //window.location = 'https://ipfs.io' + value
+
+  setTimeout(function () {
+    window.location = 'https://ipfs.io' + value
+  }, 1700)
 })
